efed46d043512d94b06622bba1475b21b232d16a
xuyisu <964092342@qq.com>
2021-11-11 10:09:28 +0800

    websocket 测试

--- fw-cloud-springboot/fw-springboot-socket/src/main/java/com/yisu/FwSocketApplication.java
+++ fw-cloud-springboot/fw-springboot-socket/src/main/java/com/yisu/FwSocketApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *
+ * 启动类
  * @Author xuyisu
  * @Date 2020/12/01
  */
